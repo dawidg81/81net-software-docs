600a08395902a0c5f0ea5c9a13ec2a7491634cf9
dawidg81 <dawidgorski.m@gmail.com>
2026-09-04 15:23:20 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>81net</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+.completed {
+    color: green;
+}
+
+.paused {
+    color: red;
+}
+
+.wip {
+    color: gray;
+}
+</style>
+</head>
+
+<body>
+
+<h1>Software</h1>
+
+<p>Welcome to 81net.</p>
+
+<h4>Since it's sometimes easier to write about
+making software rather than actually coding
+it...</h4>
+
+<p>This web page is for various articles,
+documentations, generally more web pages
+about software. In each page we have
+a program described, and how it can be
+coded. A small database of knowledge!</p>
+
+<!--<a href="classicube.html">What is ClassiCube? (This web page is archivized)</a>-->
+
+<p>Each link to a web page is colored:</p>
+<ul style="list-style-type:none;">
+<li><span class="completed">green</span>, if it's completed (always can be updated),</li>
+<li><span class="paused">red</span>, if working on it has been paused,</li>
+<li><span class="wip">gray</span>, if it's work in progress.</li>
+</ul>
+
+<ol>
+<li><a class="completed" href="fcount.html">File counter</a></li>
+<li><a class="paused" href="nbodysim.html">N-body simulation</a></li>
+<li><a class="wip" href="mccserver.html">Minecraft Classic c0.30 server</a></li>
+</ol>
+
+<hr>
+
+<a href="https://github.com/dawidg81/81net-software-docs">Contributing</a>
+
+<hr>
+
+<p><i>All material under this website is licensed under CC BY-SA 4.0 by dawidg81 and others who contribute to this site.</i></p>
+<p><i>Dates should be noted YYYY.MM.DD unless noted otherwise.</i></p>
+
+</body>
+
+</html>
